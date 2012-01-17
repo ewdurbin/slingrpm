92e8b62354392517e96a30ed35667d1b1d3354b0
Ernest W. Durbin III <ewdurbin@gmail.com>
2012-01-16 23:16:57 -0500

    working toward the client and server objects. need to add test setup

--- case_rpmpub.py
+++ case_rpmpub.py
@@ -8,14 +8,23 @@ describe "pushing a rpm package to our centralized repo":
   it "throws an exception if you dont specify the repo":
     raises Exception: SlingRPM()
   it "throws an exception if the specified repo is not an http location":
-    raises Exception: pusher = SlingRPM(targetrepo="foo/bar/bazz")
+    raises Exception: pusher = SlingRPM(targetrepo="/foo/bar/bazz/")
   it "throws an exception if the specified repo is not a yum repo":
-    raises Exception: pusher = SlingRPM(targetrepo="http://localhost:8000/foo")
-  it "likes repos with a repmod":
+    raises Exception: pusher = SlingRPM(targetrepo="http://localhost:8000/foo/")
+  it "throws an exception if the specified repo is not setup for slingrpm":
+    raises Exception: pusher = SlingRPM(targetrepo="http://mirrors.kernel.org/centos/6/cr/x86_64/")
+  it "likes repos with a repmod and slingrpm":
     pusher = SlingRPM("http://localhost:8000/")
     assert pusher
+  it "can obtain a full path to the repository from .slingrpm.conf":
+    pusher = SlingRPM("http://localhost:8000/")
+    assert os.path.isdir(pusher.targetpath)
 
 describe "receiving a package":
   it "needs a directory on the filesystem for a repo to live":
-    catcher = CatchRPM(repo='/home/pair/testrepo')
+    catcher = CatchRPM(targetrepo='/home/ernestd/testrepo')
     assert os.path.isdir(catcher.targetrepo)
+  it "throws an exception if the target repo is not setup for slingrpm":
+    raises Exception: catcher = CatchRPM(targetrepo='/home/ernestd/testrepobad')
+    
+    
--- slingrpm.py
+++ slingrpm.py
@@ -7,12 +7,20 @@ class SlingRPM:
       response = urlopen(targetrepo + 'repodata/repomd.xml')
       if response.getcode() != 200:
 	raise Exception
+      response = urlopen(targetrepo + '.slingrpm.conf')
+      if response.getcode() != 200:
+	raise Exception
+      self.targetpath = '/'
       self.targetrepo = targetrepo
 
   def push(self):
     pass
 
+import os.path
+
 class CatchRPM:
 
-  def __init__(self, repo=""):
-    pass
+  def __init__(self, targetrepo=""):
+    if not os.path.isfile(os.path.join(targetrepo, '.slingrpm.conf')):
+      raise Exception
+    self.targetrepo = targetrepo
