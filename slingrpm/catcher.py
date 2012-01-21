64eb29251defc4dda194f95a56685727854bfafa
Ernest W. Durbin III <ewdurbin@gmail.com>
2012-01-21 12:33:29 -0500

    catcher can catch a file... finally

--- slingrpm/catcher.py
+++ slingrpm/catcher.py
@@ -1,15 +1,28 @@
 import os.path
+import time
 
 import slingrpm
 
 class Catcher:
 
-  def __init__(self, targetrepo=""):
+  def __init__(self, targetrepo="", slinghost="", slingport=0, file=""):
     if not os.path.isfile(os.path.join(targetrepo, '.slingrpm.conf')):
       raise Exception
 
     self.targetrepo = targetrepo
+    self.slinghost = slinghost
+    self.slingport = slingport
     self.config = slingrpm.SlingConfig()
     self.config.read(self.targetrepo + '.slingrpm.conf')
+    self.packagedir = self.config.packagedir
+    self.package = os.path.basename(file)
+    self.dest = os.path.join(self.packagedir, self.package)
+    self.puller = slingrpm.CatcherFilePuller(self.dest, file, self.slinghost, self.slingport)
 
-
+  def pull(self):
+    self.puller.start()
+    while self.puller.status_queue.empty():
+      time.sleep(.001)
+    if not self.puller.status_queue.get() == 'FILE RECEIVED':
+      raise Exception
+    
--- slingrpm/test/case_catcher.py
+++ slingrpm/test/case_catcher.py
@@ -1,8 +1,10 @@
 import konira
+import os
 import os.path
 
 import testutils
 from slingrpm import Catcher 
+from slingrpm import SlingerFileServer
 
 describe "receiving a package with Catcher":
  
@@ -13,33 +15,48 @@ describe "receiving a package with Catcher":
     self.repopath = os.path.join(os.getcwd(),'testarea/repo/')
 
   before each:
-    pass
+    self.slinger = SlingerFileServer(os.path.join(os.getcwd(), 'slingrpm/test'))
+    self.slinger.start()
+    self.port = self.slinger.port
+    self.filetoget = os.path.join(os.getcwd(), 'slingrpm/test/empty-0-1.i386.rpm')
+    self.nonfiletoget = os.path.join(os.getcwd(), 'slingrpm/test/empty-0-10.i386.rpm')
  
   it "throws an exception if the specified repo is not on the filesystem":
-    raises Exception: catcher = Catcher(targetrepo=self.badrepopath)
+    raises Exception: catcher = Catcher(targetrepo=self.badrepopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
 
-  it "throws an exception if no repo is specified":
-    raises Exception: catcher = Catcher()
+  it "accepts a targetrepo, host, port and file for catching an RPM":
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
+    assert catcher
 
   it "remembers the path to the targetrepo":
-    catcher = Catcher(targetrepo=self.repopath)
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
     assert catcher.targetrepo == self.repopath 
 
   it "can obtain a full path to the repository from .slingrpm.conf":
-    catcher = Catcher(targetrepo=self.repopath)
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
     assert os.path.isdir(catcher.config.repolocation)
 
   it "can obtain a subdirectory in the repository for pushing packages into":
-    catcher = Catcher(targetrepo=self.repopath)
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
     assert catcher.config.packagedir == os.path.join(catcher.config.repolocation, "")
 
   it "can obtain a set of parameters for running createrepo on the target repo":
-    catcher = Catcher(targetrepo=self.repopath)
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
     assert catcher.config.createrepoopts == "--update --excludes .slingrpm.conf --checksum sha" 
 
+  it "obtains the file when pull method is called":
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.filetoget)
+    catcher.pull()
+    assert os.path.isfile(os.path.join(catcher.packagedir, os.path.basename(self.filetoget)))
+    os.remove(os.path.join(catcher.packagedir, os.path.basename(self.filetoget)))
+
+  it "raises an exception if pull method is called on nonexistant file":
+    catcher = Catcher(targetrepo=self.repopath, slinghost='localhost', slingport=self.port, file=self.nonfiletoget)
+    raises Exception: catcher.pull()
+
   after each:
-   pass
-    
+    self.slinger.stop()
+
   after all:
     testutils.teardownrepos()
 
