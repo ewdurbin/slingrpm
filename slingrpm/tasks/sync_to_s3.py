07703fbfc6597abdd4d9d5f73bcc902b3381f747
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-08-18 14:43:22 -0400

    add sync to s3 task

---
+++ slingrpm/tasks/sync_to_s3.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+import os
+
+from simpleflock import SimpleFlock
+
+from slingrpm.celery import CELERY_APP
+from slingrpm.utils.s3 import S3Syncer
+
+
+@CELERY_APP.task(name="slingrpm.tasks.sync_to_s3")
+def sync_to_s3(repository_dir, bucket_name, prefix):
+    try:
+        with SimpleFlock(os.path.join(repository_dir, '.slingrpm.lock'), timeout=3):
+            syncer = S3Syncer(repository_dir, bucket_name, prefix)
+            syncer.sync_s3()
+    except (IOError) as exc:
+        raise update_repo.retry(exc=exc, max_retries=1)
