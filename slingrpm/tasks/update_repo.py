98debf7f7634aeadfdd1e82221eef1fd81044c61
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-08-18 16:01:48 -0400

    enable sync_to_s3 as side effect of repository updates

--- slingrpm/tasks/update_repo.py
+++ slingrpm/tasks/update_repo.py
@@ -8,12 +8,17 @@ from pyrpm.tools.createrepo import YumRepository
 from simpleflock import SimpleFlock
 
 from slingrpm.celery import CELERY_APP
+from slingrpm.repo_config import load_private_config
+from slingrpm.tasks import sync_to_s3
 
 
 @CELERY_APP.task(name="slingrpm.tasks.update_repo")
 def update_repo(repository_dir):
     try:
-        with SimpleFlock(os.path.join(repository_dir, '.slingrpm.lock'), timeout=3):
+        with SimpleFlock(os.path.join(repository_dir, '.slingrpm.lock'),
+                         timeout=3):
+            repo_config = load_private_config(os.path.join(repository_dir,
+                                                           '.slingrpm.cfg'))
             rpms = []
             for root, dirs, files in os.walk(repository_dir):
                 for filename in files:
@@ -29,5 +34,10 @@ def update_repo(repository_dir):
                     repository.add_package(pkg)
 
             repository.save()
+            if repo_config.getboolean('private', 's3_sync'):
+                bucket_name = repo_config.get('private', 's3_sync_bucket')
+                prefix = repo_config.get('private', 's3_sync_prefix')
+                job_args = (repository_dir, bucket_name, prefix)
+                sync_to_s3.apply_async(args=job_args)
     except (IOError) as exc:
         raise update_repo.retry(exc=exc, max_retries=1)
