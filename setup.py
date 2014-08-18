6942e87a304e6be3f9aaf06f555e69f3187638e5
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-08-17 23:09:18 -0400

    fix missing utils module

--- setup.py
+++ setup.py
@@ -12,13 +12,14 @@ test_requirements = []
 
 setup(
     name='slingrpm',
-    version='1.0.0a1',
+    version='1.0.0a2',
     license='MIT',
     author="Ernest W. Durbin III",
     author_email='ewdurbin@gmail.com',
     description='a lightweight manager for pushing, removing, and promoting rpms in a remote yum repository',
     url='https://github.com/ewdurbin/slingrpm',
     packages=['slingrpm',
+              'slingrpm.utils',
               'slingrpm.server',
               'slingrpm.tasks'],
     scripts=['scripts/slingrpm'],
